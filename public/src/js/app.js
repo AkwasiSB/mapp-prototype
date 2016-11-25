cfdb3bc7fff7ccb0f9e07de82dc9e6b1965d342e
AkwasiSB <akwasisefaboakye@yahoo.com>
2016-11-25 14:27:36 +0000

    improved mapp home design

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,11 +1,14 @@
 /* angular.module(wistiaApp) is a global place for creating, registering and retrieving 
 Angular modules */
-var mappApp = angular.module('mappApp', ['ngRoute', 'ngAnimate', 'ngMessages', 'ngDialog', 'angular-svg-round-progressbar', 'mapp-upload', 'mapp.filters', 'homePage.controller', 'userAccount.controller', 'profilePage.controller']);
+var mappApp = angular.module('mappApp', ['ngRoute', 'ngAnimate', 'ngMessages', 'ngDialog', 'angular-svg-round-progressbar', 'angular-loading-bar', 'mapp-upload', 'mapp.filters', 'homePage.controller', 'userAccount.controller', 'profilePage.controller']);
 
 
 
 // app config phase
-mappApp.config(['$routeProvider', function ($routeProvider) {
+mappApp.config(['$routeProvider', 'cfpLoadingBarProvider', function ($routeProvider, cfpLoadingBarProvider) {
+	// configure loading bar to hide spinner
+	cfpLoadingBarProvider.includeSpinner = false;
+
 	// set up page routing
 	$routeProvider
 	.when('/', {
--- public/src/js/controllers/profilepage-controller.js
+++ public/src/js/controllers/profilepage-controller.js
@@ -2,152 +2,163 @@ angular.module('profilePage.controller', [])
 
 
 
-.controller('uploadModalCtrl', ['$scope', '$interval', '$timeout', function($scope, $interval, $timeout) {
-	// store data from jquery-file-upload plugin when uploading a file
-    var uploadData = {};
-
-    var intervar;
-
-    // for toggling the visibility state of dom elements
-	$scope.startUpload = false;
-    $scope.startProc = false;
-
-	// progress and loop bar percentage
-	$scope.cProgress = 0;
-    $scope.cLoop = 0;
-    $scope.cTimer = 0;
-
-	function roundLoader() {
-        intervar = $interval(function() {
-            $scope.cLoop += 1;
-            $scope.cTimer += 1;
-
-            if ($scope.cLoop == 100) {
-                $scope.cLoop = 0;
-            }
-        }, 1000);
-    }
-
-    // listen for the start of an upload
-    $scope.$on('mappUploadStart', function(e, data) {
-    	uploadData = data;
-    	$scope.startUpload = true;
-    	$scope.$digest();
-    });
-
-    // listen for upload progress
-    $scope.$on('mappUploadProgress', function(e, data) {
-    	$scope.cProgress = data;
-
-        if (data == 100) {
-            $scope.startUpload = false;
-            $scope.startProc = true;
-        }
-
-        $scope.$digest();
-
-        roundLoader();
-    });
-
-    // listen for successful upload
-    $scope.$on('mappUploadSuccess', function(e, data) {
-        $scope.$apply(function() {
-            $scope.startUpload = false;
-            $scope.startProc = false;
-            $scope.cProgress = 0;
-            $scope.cLoop = 0;
-            $scope.cTimer = 0;
-
-            $interval.cancel(intervar);
+.controller('profilePageCtrl', ['$scope', '$window', '$timeout', function($scope, $window, $timeout) {
+	var map = new google.maps.Map(document.getElementById('gmap'), {
+        	center: {lat: 54.582, lng: -4.120},
+        	zoom: 6,
+        	draggable: false,
+      		mapTypeControl: false,
+      		scaleControl: false,
+      		navigationControl: true,
+      		streetViewControl: false
         });
-    });
 
-    // listen for error during an upload
-    $scope.$on('mappUploadError', function(e, data) {
-        var message;
+	// Minimap control
+	var miniMapControlDiv = document.createElement('div');
 
-        $scope.startUpload = false;
-        $scope.startProc = false;
-        $scope.cProgress = 0;
-        $scope.cLoop = 0;
-        $scope.cTimer = 0;
+	var miniMapButton = document.createElement('button');;
+	miniMapButton.style.backgroundColor = '#fff';
+	miniMapButton.style.cursor = 'pointer';
+    miniMapButton.style.boxShadow = '0px 1px 4px -1px rgba(0, 0, 0, 0.3)';
+    miniMapButton.style.borderRadius = '2px';
+	miniMapButton.style.width = '75px';
+	miniMapButton.style.height = '75px';
+    miniMapButton.style.border = '3px solid #fff';
+    miniMapButton.style.outline = 'none';
+    miniMapButton.style.padding = '0';
+	miniMapButton.style.marginLeft = '10px';
+    miniMapButton.style.marginBottom = '10px';
+	miniMapButton.style.top = '0px';
+	miniMapButton.title = 'Satellite';
+	miniMapControlDiv.appendChild(miniMapButton);
 
-        if (intervar) {
-            $interval.cancel(intervar);
-        }
+	var miniMapBox = document.createElement('div');
+	miniMapBox.style.height = '100%';
+	miniMapBox.style.zIndex = '9999';
+	miniMapButton.appendChild(miniMapBox);
 
-        if (data == 'error') {
-            message = 'An unknown error occured while uploading the file. Retry';
-        }
-        else if (data == 'abort') {
-            message = 'File upload cancelled';
-        }
-        else if (data == 'filetypeErr') {
-            message = 'Filetype not supported';
-        }
-        else if (data == 'Payload Too Large') {
-            message = 'Filesize has exceeded max limit';
-        }
-
-        $('.notification').css({ background: 'red', display: 'block' });
-        $('.notifytext').html(message);
-
-        $timeout(function () {
-            $('.notification').css({ background: 'none', display: 'none' });
-            $('.notifytext').html('');
-        }, 5000);
-    });
-
-    // styles for styling the round progressbar
-    $scope.getStyle = function() {
-        var transform = 'translateY(-50%) translateX(-50%)';
-
-        return {
-            'top': '50%',
-            'bottom': 'auto',
-            'left': '50%',
-            'transform': transform,
-            '-moz-transform': transform,
-            '-webkit-transform': transform,
-            'font-size': 100/5.2 + 'px'
-        };
-    };
-
-    // function for cancelling an ongoing upload if any
-    $scope.cancelUpload = function() {
-    	uploadData.jqXHR.abort();
-    };
-}])
-
-
-
-.controller('profilePageCtrl', ['$scope', '$window', '$timeout', 'ngDialog', function($scope, $window, $timeout, ngDialog) {
-	var ngDiag;
-    $scope.pcModels = [];
-
-    $scope.showUploadModal = function() {
-        ngDiag = ngDialog.open({
-        	template: 'templateId',
-        	controller: 'uploadModalCtrl',
-        	className: 'ngdialog-theme-default'
+	var minimap = new google.maps.Map(miniMapBox, {
+			mapTypeId: google.maps.MapTypeId.SATELLITE,
+        	center: {lat: 54.582, lng: -4.120},
+        	zoom: 6,
+        	draggable: false,
+      		mapTypeControl: false,
+      		scaleControl: false,
+      		navigationControl: false,
+      		streetViewControl: false,
+      		zoomControl: false,
         });
+
+	miniMapButton.addEventListener('click', function() {
+	    console.log('miniMapButton');
+	});
+
+	map.controls[google.maps.ControlPosition.BOTTOM_LEFT].push(miniMapControlDiv);
+
+	google.maps.event.addListener(minimap, 'idle', function(){
+	    minimap.setCenter({lat: 54.582, lng: -4.120});
+	    minimap.setZoom(4);
+		google.maps.event.trigger(minimap, 'resize');
+	});
+
+	// My location control
+	var locationControlDiv = document.createElement('div');
+
+	var locationButton = document.createElement('button');
+    locationButton.style.backgroundColor = '#fff';
+    locationButton.style.border = 'none';
+    locationButton.style.outline = 'none';
+    locationButton.style.width = '28px';
+    locationButton.style.height = '28px';
+    locationButton.style.borderRadius = '2px';
+    locationButton.style.boxShadow = '0 1px 4px rgba(0, 0, 0, 0.3)';
+    locationButton.style.cursor = 'pointer';
+    locationButton.style.marginRight = '10px';
+    locationButton.style.padding = '0';
+    locationButton.title = 'Your Location';
+    locationControlDiv.appendChild(locationButton);
+
+    var locationImage = document.createElement('div');
+    locationImage.style.margin = '5px';
+    locationImage.style.width = '24px';
+    locationImage.style.height = '26px';
+    locationImage.style.backgroundImage = 'url(../../images/location-marker.png)';
+    locationImage.style.backgroundSize = '14px 18px';
+    locationImage.style.backgroundPosition = '1.5px 0';
+    locationImage.style.backgroundRepeat = 'no-repeat';
+    locationButton.appendChild(locationImage);
+
+	locationButton.addEventListener('click', function() {
+	    console.log('locationButton');
+	});
+
+	map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(locationControlDiv);
+
+	// Map Tools control
+	var mapToolsControlDiv = document.createElement('div');
+
+	var mapToolsButton = document.createElement('button');
+    mapToolsButton.style.backgroundColor = '#fff';
+    mapToolsButton.style.border = 'none';
+    mapToolsButton.style.outline = 'none';
+    mapToolsButton.style.width = '158px';
+    mapToolsButton.style.height = '30px';
+    mapToolsButton.style.borderRadius = '2px';
+    mapToolsButton.style.boxShadow = '0 1px 4px rgba(0, 0, 0, 0.3)';
+    mapToolsButton.style.cursor = 'pointer';
+    mapToolsButton.style.marginRight = '10px';
+    mapToolsButton.style.marginBottom = '10px';
+    mapToolsButton.style.padding = '0';
+    mapToolsButton.title = 'Show Imagery';
+    mapToolsControlDiv.appendChild(mapToolsButton);
+
+    var mapToolsImage = document.createElement('div');
+    mapToolsImage.style.margin = '5px';
+    mapToolsImage.style.width = '148px';
+    mapToolsImage.style.height = '28px';
+    mapToolsImage.style.backgroundImage = 'url(../../images/icon-sprite.png), url(../../images/caret-up.png)';
+    mapToolsImage.style.backgroundSize = '128px 28px, 11px 8px';
+    mapToolsImage.style.backgroundPosition = '0 -3px, 134px 6px';
+    mapToolsImage.style.backgroundRepeat = 'no-repeat, no-repeat';
+    mapToolsButton.appendChild(mapToolsImage);
+
+	mapToolsButton.addEventListener('click', function() {
+	    console.log('mapToolsButton');
+	});
+
+	map.controls[google.maps.ControlPosition.BOTTOM_RIGHT].push(mapToolsControlDiv);
+
+    $scope.showMenu = function() {
+    	$('.settings-background').addClass('open');
+    	$('.settings').addClass('open');
     };
 
-    $scope.loadPCModel = function(modelURL) {
-        $window.location.assign(modelURL);
+    $scope.hideMenu = function() {
+    	$('.settings-background').removeClass('open');
+    	$('.settings').removeClass('open');
     };
 
-    // listen for successful upload
-    $scope.$on('mappUploadSuccess', function(e, data) {
-        var model = data;
-
-        $scope.pcModels.push(model);
-
-        ngDiag.close();
+    $('.settings').on('click', function(e) {
+    	e.stopPropagation();
+    	e.preventDefault();
     });
 
-    // listen for error during an upload
-    $scope.$on('mappUploadError', function(e, data) {
-        ngDiag.close();
-    });
+	$('ul.dropdown-menu').on('click', function(event) {
+	    var events = $._data(document, 'events') || {};
+	    events = events.click || [];
+
+	    for(var i = 0; i < events.length; i++) {
+	        if(events[i].selector) {
+				if($(event.target).is(events[i].selector)) {
+	                events[i].handler.call(event.target, event);
+	            }
+
+	            $(event.target).parents(events[i].selector).each(function() {
+	                events[i].handler.call(this, event);
+	            });
+	        }
+	    }
+
+	    event.stopPropagation();
+	});
 }]);
